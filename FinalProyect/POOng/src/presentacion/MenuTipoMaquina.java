4342737da64cd2d9b4ffbbf8a969d70193cad35e
SoyTiyi <santiago.marmartinez@gmail.com>
2020-04-21 17:28:19 -0500

    Se añadio fondo para background ventana tipo

--- FinalProyect/POOng/src/presentacion/MenuTipoMaquina.java
+++ FinalProyect/POOng/src/presentacion/MenuTipoMaquina.java
@@ -1,5 +1,6 @@
 package src.presentacion;
 import javax.swing.*;
+import java.awt.*;
 /**
  * Esta clase nos muestra la interfaz grafica para escoger el tipo de maquina 
  */
@@ -10,8 +11,29 @@ public class MenuTipoMaquina extends JFrame{
     /**
      * Este es el constructor de la clase
      */
+    private JLabel backGround;
     public MenuTipoMaquina(){
+        getContentPane().setBackground(Color.black);
+        prepareElementos();
+    }
 
+    /**
+     * Este metodo nos prepara los elementos con los que interactua el usuario
+     */
+    private void prepareElementos(){
+        setLayout(null);
+        setTitle("Modos");
+        setIconImage(new ImageIcon(getClass().getResource("images/icon2.png")).getImage());
+        Toolkit t = Toolkit.getDefaultToolkit();
+        Dimension d = t.getScreenSize();
+        setBounds(0,0,d.width/2+50,d.height/2+50);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        ImageIcon back = new ImageIcon("C:/Users/santi/Desktop/POOB/FinalProyect/POOng/src/presentacion/images/background1.png");
+        backGround = new JLabel(back);
+        backGround.setBounds(0,0,d.width/2+50,d.height/2+50);
+        backGround.setLayout(null);
+        add(backGround);
     }
 
 
